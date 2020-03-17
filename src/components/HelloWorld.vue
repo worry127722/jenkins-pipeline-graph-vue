0776a8310f01f94dd0e5d49fe7b7f9fb3a6056b1
wangrui09 <wangrui09@kuaishou.com>
2020-03-17 16:04:20 +0800

    增加实例

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,23 @@
 <template>
     <div class="hello">
-        <h1>{{ msg }}</h1>
+        <h1>JenkinsPipelineGraphVue</h1>
 
         <JenkinsPipelineGraphVue
             :stages='stage'
             :layout='layout'
+            :onNodeClick='nodeClick'
+            :selectedStage='selectedNode'
         />
 
-        <p>22</p>
-        <p>
-            For a guide and recipes on how to configure / customize this project,<br>
-            check out the
-            <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-        </p>
-        <h3>Installed CLI Plugins</h3>
-        <ul>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank"
-                   rel="noopener">babel</a></li>
-            <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank"
-                   rel="noopener">eslint</a></li>
-        </ul>
-        <h3>Essential Links</h3>
-        <ul>
-            <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-            <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-            <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-            <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-            <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-        </ul>
-        <h3>Ecosystem</h3>
-        <ul>
-            <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-            <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-            <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank"
-                   rel="noopener">vue-devtools</a></li>
-            <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-        </ul>
     </div>
 </template>
 
 <script>
-    // import SvgSpinner from "./support/SvgSpinner";
     import JenkinsPipelineGraphVue from "./PipelineGraph";
-    // import {nodeStrokeWidth} from "./support/StatusIcons.js";
-    // import {Result} from "./PipelineGraphModel";
-
-    // import '../assets/styles/css/main.css'
 
     export default {
         name: 'HelloWorld',
         components: {
-            // SvgSpinner,
             JenkinsPipelineGraphVue
         },
         data() {
@@ -74,16 +40,16 @@
                     "id": 1587,
                     "type": "STAGE"
                 }, {
-                    "name": "流程",
+                    "name": "流程2",
                     "children": [{
-                        "name": "子流程",
+                        "name": "子流程2-1",
                         "children": [],
                         "state": "SUCCESS",
                         "completePercent": 50,
                         "id": 1589,
                         "type": "PARALLEL"
                     }, {
-                        "name": "子流程",
+                        "name": "子流程2-2",
                         "children": [],
                         "state": "SUCCESS",
                         "completePercent": 50,
@@ -95,44 +61,30 @@
                     "id": 1588,
                     "type": "STAGE"
                 }, {
-                    "name": "流程",
-                    "children": [],
-                    "state": "SUCCESS",
-                    "completePercent": 50,
-                    "id": 1591,
-                    "type": "STAGE"
-                }, {
-                    "name": "流程",
-                    "children": [],
-                    "state": "SUCCESS",
-                    "completePercent": 50,
-                    "id": 1594,
-                    "type": "STAGE"
-                }, {
-                    "name": "流程",
+                    "name": "流程3",
                     "children": [],
                     "state": "PAUSED",
                     "completePercent": 50,
                     "id": 1595,
                     "type": "STAGE"
                 }, {
-                    "name": "流程",
+                    "name": "流程4",
                     "children": [],
                     "state": "not_built",
                     "completePercent": 50,
                     "id": 1596,
                     "type": "STAGE"
                 }, {
-                    "name": "流程",
+                    "name": "流程5",
                     "children": [{
-                        "name": "子流程",
+                        "name": "子流程5-1",
                         "children": [],
                         "state": "not_built",
                         "completePercent": 50,
                         "id": 1598,
                         "type": "PARALLEL"
                     }, {
-                        "name": "子流程",
+                        "name": "子流程5-2",
                         "children": [],
                         "state": "not_built",
                         "completePercent": 50,
@@ -144,18 +96,38 @@
                     "id": 1597,
                     "type": "STAGE"
                 }, {
-                    "name": "流程",
+                    "name": "流程6",
                     "children": [],
                     "state": "not_built",
                     "completePercent": 50,
                     "id": 1600,
                     "type": "STAGE"
-                }]
+                }],
+                selectedNode: {
+                    "displayName": "自测1",
+                    "displayDescription": null,
+                    "durationInMillis": 2284,
+                    "result": "SUCCESS",
+                    "startTime": "2020-03-11T17:29:11.972+0800",
+                    "state": "FINISHED",
+                    "type": "STAGE",
+                    "firstParent": null,
+                    "id": 1587,
+                    "desc": "自测阶段，需要MergeRequest中有\"自测完成\"标签"
+                }
             }
         },
         props: {
             msg: String
         },
+        methods: {
+            nodeClick(nodeName, id) {
+                console.log(nodeName)
+                console.log(id)
+                alert('节点名称：' + nodeName)
+                alert('节点id：' + id)
+            },
+        }
     }
 </script>
 
